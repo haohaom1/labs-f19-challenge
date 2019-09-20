cc1969ff50d64b6324f2a90b48b7faddff36393b
Mike Fu <xuanhaofu@gmail.com>
2019-09-20 16:04:04 -0400

    finished project

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, escape, url_for
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/pokemon/<entry>')
+def pokemon_identify(entry):
+    req = requests.get('https://pokeapi.co/api/v2/pokemon/{}'.format(entry))
+    data = req.json()
+    # return '{}\'s profile'.format(escape(entry))
+    
+    # entered int
+    if entry.isdigit():      
+        name = data['name']
+        return 'The pokemon with id {} is {}'.format(entry, name)
+    # entered name
+    else:           
+        pokemonID = data['id']
+        return '{} has id {}'.format(entry, pokemonID)
 
 
 if __name__ == '__main__':
